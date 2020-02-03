8f187fc5bfc3311638876352f4ac1982746593e7
Peter Harman <peter.harman@deltatheta.com>
2020-02-03 09:38:41 +0000

    Added frame attribute to Pose type

--- jsdf/src/main/java/tech/cae/jsdf/types/Pose.java
+++ jsdf/src/main/java/tech/cae/jsdf/types/Pose.java
@@ -7,23 +7,35 @@ package tech.cae.jsdf.types;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlText;
 import org.apache.commons.math3.geometry.euclidean.threed.RotationConvention;
 import org.apache.commons.math3.geometry.euclidean.threed.RotationOrder;
+import tech.cae.jsdf.types.SDFArrayType.SDFArrayDeserializer;
+import tech.cae.jsdf.types.SDFArrayType.SDFArraySerializer;
 
 /**
  *
  * @author peter
  */
-@JsonSerialize(using = Pose.PoseSerializer.class)
-@JsonDeserialize(using = Pose.PoseDeserializer.class)
-public class Pose extends SDFArrayType {
+public class Pose {
+
+    @JacksonXmlProperty(isAttribute = true, localName = "frame")
+    private PoseContent frame;
+
+    @JacksonXmlText
+    private PoseContent content;
+
+    public Pose() {
+
+    }
 
     public Pose(double... data) {
-        super(data);
+        this.content = new PoseContent(data);
     }
 
     public Pose(String str) {
-        super(str);
+        this.content = new PoseContent(str);
     }
 
     public static Pose fromCommonsMath(org.apache.commons.math3.geometry.euclidean.threed.Vector3D v3d,
@@ -33,20 +45,34 @@ public class Pose extends SDFArrayType {
         System.arraycopy(rotation.getAngles(RotationOrder.XYZ, RotationConvention.FRAME_TRANSFORM), 0, a6, 3, 3);
         return new Pose(a6);
     }
-    
-    static class PoseSerializer extends SDFArraySerializer<Pose> {
+
+    @JsonSerialize(using = Pose.PoseSerializer.class)
+    @JsonDeserialize(using = Pose.PoseDeserializer.class)
+    public static class PoseContent extends SDFArrayType {
+
+        public PoseContent(double... data) {
+            super(data);
+        }
+
+        public PoseContent(String str) {
+            super(str);
+        }
+    }
+
+    static class PoseSerializer extends SDFArraySerializer<PoseContent> {
 
         public PoseSerializer() {
-            super(Pose.class);
+            super(PoseContent.class);
         }
 
     }
 
-    static class PoseDeserializer extends SDFArrayDeserializer<Pose> {
+    static class PoseDeserializer extends SDFArrayDeserializer<PoseContent> {
 
         public PoseDeserializer() {
-            super(Pose.class);
+            super(PoseContent.class);
         }
+
     }
 
 }

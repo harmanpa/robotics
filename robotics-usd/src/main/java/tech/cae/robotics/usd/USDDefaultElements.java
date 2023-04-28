d1cbd3becfc1d492c1f51e1c27e02d98e674569d
irminasciezor <irminasciezor@gmail.com>
2023-04-28 11:56:02 +0200

    created default elements class

---
+++ robotics-usd/src/main/java/tech/cae/robotics/usd/USDDefaultElements.java
@@ -0,0 +1,23 @@
+package tech.cae.robotics.usd;
+
+public class USDDefaultElements {
+    private String stage;
+
+    private String defaultPrim;
+
+    public String getStage() {
+        return stage;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+    public String getDefaultPrim() {
+        return defaultPrim;
+    }
+
+    public void setDefaultPrim(String defaultPrim) {
+        this.defaultPrim = defaultPrim;
+    }
+}

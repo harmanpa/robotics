4913b02514ca6321c1057c36bc45540a9681c2da
Irmina Ściężor <irmina.sciezor@futurum.tech>
2023-05-16 16:05:10 +0200

    reduced number of fields in USDRobot, using visual object to get origin and color in mustache template

--- robotics-usd/src/main/java/tech/cae/robotics/usd/USDRobot.java
+++ robotics-usd/src/main/java/tech/cae/robotics/usd/USDRobot.java
@@ -1,15 +1,22 @@
 package tech.cae.robotics.usd;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import tech.cae.robotics.urdf.*;
 
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
+@Getter
+@Setter
 public class USDRobot {
     public static File tempDir = new File("temp.py");
     public USDRobot(){};
     protected String physicsScene;
+    protected String mass;
+    protected String mesh;
     protected String meshAttribute;
     protected String joint;
     protected String parentJoint;
@@ -23,6 +30,7 @@ public class USDRobot {
     protected String jointAxis;
     protected String inertiaAttribute;
     protected String massAttribute;
+    @Setter(AccessLevel.NONE)
     protected String inertiaCoordinatesArray;
     protected String originXYZ;
     protected String originRPY;
@@ -30,210 +38,11 @@ public class USDRobot {
     protected String collisionMesh;
     protected String collisionOriginXYZ;
     protected String collisionOriginRPY;
-    protected String visualOriginXYZ;
-    protected String visualOriginRPY;
-    protected String colorAttribute;
     protected List<Visual> visualList;
-
-
-    public void setChildPrim(String childPrim) {
-        this.childPrim = childPrim;
-    }
-
-    public String getChildPrim() {
-        return childPrim;
-    }
-
-    public void setPhysicsScene(String physicsScene) {
-         this.physicsScene = physicsScene;
-    }
-
-    public String getPhysicsScene() {
-        return physicsScene;
-    }
-
-    public void setMesh(String meshAttribute) {
-        this.meshAttribute = meshAttribute;
-    }
-
-    public String getMesh() {
-        return meshAttribute;
-    }
-
-    public void setJoint(String joint) {
-        this.joint = joint;
-    }
-
-    public String getJoint() {
-        return joint;
-    }
-
-    public void setSphereAttribute(String sphereAttribute) {
-        this.sphereAttribute = sphereAttribute;
-    }
-
-    public String getSphereAttribute() {
-        return sphereAttribute;
-    }
-
-    public String getInertiaAttribute() {
-        return inertiaAttribute;
-    }
-
-    public void setInertiaAttribute(String inertiaAttribute) {
-        this.inertiaAttribute = inertiaAttribute;
-    }
-
-    public void setMass(Mass mass) {
-        this.massAttribute = String.valueOf(mass.getValue());
-    }
-
-    public String getMass() {
-        return massAttribute;
-    }
-
-    public String getParentJoint() {
-        return parentJoint;
-    }
-
-    public void setParentJoint(String parentJoint) {
-        this.parentJoint = parentJoint;
-    }
-
-    public String getLowerLimit() {
-        return lowerLimit;
-    }
-
-    public void setLowerLimit(String lowerLimit) {
-        this.lowerLimit = lowerLimit;
-    }
-
-    public String getUpperLimit() {
-        return upperLimit;
-    }
-
-    public void setUpperLimit(String upperLimit) {
-        this.upperLimit = upperLimit;
-    }
-
-    public String getXYZAxis() {
-        return XYZAxis;
-    }
-
-    public void setXYZAxis(String XYZAxis) {
-        this.XYZAxis = XYZAxis;
-    }
-
-    public String getRPYAxis() {
-        return RPYAxis;
-    }
-
-    public void setRPYAxis(String RPYAxis) {
-        this.RPYAxis = RPYAxis;
-    }
-
-    public String getInertiaCoordinatesArray() {
-        return inertiaCoordinatesArray;
-    }
 
     public void setInertiaCoordinatesArray(Inertia inertia) {
         this.inertiaCoordinatesArray = Arrays.toString(new Double[]{inertia.getIxx(), inertia.getIxy(),
                 inertia.getIxz(), inertia.getIyy(), inertia.getIyz(), inertia.getIzz()});
     }
 
-    public String getOriginXYZ() {
-        return originXYZ;
-    }
-
-    public void setOriginXYZ(String originXYZ) {
-        this.originXYZ = originXYZ;
-    }
-
-    public String getOriginRPY() {
-        return originRPY;
-    }
-
-    public void setOriginRPY(String originRPY) {
-        this.originRPY = originRPY;
-    }
-
-    public String getChildJoint() {
-        return childJoint;
-    }
-
-    public void setChildJoint(String childJoint) {
-        this.childJoint = childJoint;
-    }
-
-    public String getJointAxis() {
-        return jointAxis;
-    }
-
-    public void setJointAxis(String jointAxis) {
-        this.jointAxis = jointAxis;
-    }
-
-    public boolean isRigidBody() {
-        return isRigidBody;
-    }
-
-    public void setRigidBody(boolean rigidBody) {
-        isRigidBody = rigidBody;
-    }
-
-    public String getCollisionMesh() {
-        return collisionMesh;
-    }
-
-    public void setCollisionMesh(String collisionMesh) {
-        this.collisionMesh = collisionMesh;
-    }
-
-    public String getColorAttribute() {
-        return colorAttribute;
-    }
-
-    public void setColorAttribute(String colorAttribute) {
-        this.colorAttribute = colorAttribute;
-    }
-
-    public String getCollisionOriginXYZ() {
-        return collisionOriginXYZ;
-    }
-
-    public void setCollisionOriginXYZ(String collisionOriginXYZ) {
-        this.collisionOriginXYZ = collisionOriginXYZ;
-    }
-
-    public String getCollisionOriginRPY() {
-        return collisionOriginRPY;
-    }
-
-    public void setCollisionOriginRPY(String collisionOriginRPY) {
-        this.collisionOriginRPY = collisionOriginRPY;
-    }
-
-    public String getVisualOriginXYZ() {
-        return visualOriginXYZ;
-    }
-
-    public void setVisualOriginXYZ(String visualOriginXYZ) {
-        this.visualOriginXYZ = visualOriginXYZ;
-    }
-
-    public String getVisualOriginRPY() {
-        return visualOriginRPY;
-    }
-
-    public void setVisualOriginRPY(String visualOriginRPY) {
-        this.visualOriginRPY = visualOriginRPY;
-    }
-
-    public List<Visual> getVisualList() {
-        return visualList;
-    }
-
-    public void setVisualList(List<Visual> visualList) {
-        this.visualList = visualList;
-    }
 }
